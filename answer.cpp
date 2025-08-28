e5c7caf5f0aad6bd1e0b8310652380d4ffdf4e07
Mehrradin <mehrradin@gmail.com>
2025-08-28 15:09:03 +0330

    https://quera.org/problemset/72882
    https://quera.org/problemset/72882

--- answer.cpp
+++ answer.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <string>
-#include <set>
-using namespace std; 
+using namespace std ; 
 int main(){
+  int n , m ; 
+  int meat = 0 ; 
+  int meat2 = 0  ;
+  int limo = 0 ; 
+  int sabzi = 0 ; 
+  cin >> n >> m ; 
+  for (int i = 1 ; i<=  n ; i ++ ){
+    for (int i = 1 ; i<= m ; i++){
+      char x ; 
+      cin >> x ; 
+      if (x == 'o'){
+        limo += 1 ;
+      }
+      if(x == '.'){
+        sabzi += 1 ; 
+      }
+      if(x == '*'){
+        meat += 1 ;
+      }
+    }
+  }
+    for (int i = 1 ; i<=  n ; i ++ ){
+    for (int i = 1 ; i<= m ; i++){
+      char x ; 
+      cin >> x ; 
+      if (x == 'o'){
+        limo += 1 ;
+      }
+      if(x == '.'){
+        sabzi += 1 ; 
+      }
+      if(x == '*'){
+        meat2 += 1 ;
+      }
+    }
+  }
 
-  string name ; 
-  int num  = 0 ; 
-  cin >> name ; 
-  set <char> myset  ; 
-  for (char x :  name){
-    myset.insert(x);
-  }
-  for (char x : myset){
-    num += 1 ; 
-  }
-  if (num % 2 == 0){
-    cout << "CHAT WITH HER!"  ; 
-  }
-  else {
-    cout << "IGNORE HIM!"  ;
-  }
+  cout << meat << " " << meat2 ;  
 
   return 0 ; 
 }
